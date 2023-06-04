efce4c617ed9e25e4cab6b3c185fa19e9ef4a3b7
Akash Jyoti Sahoo <akashvana2001@gmail.com>
2023-06-04 21:53:32 +0530

    changed network to sepolia

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,15 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env"});
 
-const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    goerli: {
-      url: QUICKNODE_HTTP_URL,
+    sepolia: {
+      url: ALCHEMY_HTTP_URL,
       accounts: [PRIVATE_KEY],
     }
   }
