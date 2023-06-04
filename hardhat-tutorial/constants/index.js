446f2b5ca838fa2d0cad15a3ecaf030c54d53b60
Akash Jyoti Sahoo <akashvana2001@gmail.com>
2023-06-04 21:36:00 +0530

    updated contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x98fE6B7bd289CcdaB22f8C2157313115f0e92b03";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-rho-two.vercel.app/api/";
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
