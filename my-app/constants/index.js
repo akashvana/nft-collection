cd0ba89bc9c86588d946ba8f6c249bb0b1355848
Akash Jyoti Sahoo <akashvana2001@gmail.com>
2023-06-04 21:57:13 +0530

    resolved minor bug

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -581,4 +581,4 @@ export const abi = [
       "type": "receive"
     }
   ];
-export const NFT_CONTRACT_ADDRESS = "0x080caC60D09CD149CeBCB31C44bAA8EAA92f96cf"
+export const NFT_CONTRACT_ADDRESS = "0x99F3cbD684CBeF4C8fcCe37b2A65E2744bdAca57"
